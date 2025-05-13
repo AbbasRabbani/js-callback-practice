d5f7700ca6d4821a0704d5a098c443cae6b1cbc4
Abbas Rabbani <mail.abbas.rabbani@gmail.com>
2025-05-13 15:00:35 +0200

    Creating a miniblog branch

---
+++ API_Practice_set/fetch_API.js
@@ -0,0 +1,5 @@
+// 1. Create a free API from the Internet and feed your APP with Live data
+// 2. Create a Note saving App which stores your Note to a local storage
+// 3. Repeat the last Question and fetch the NOTE which was saved
+// 4. Delete the Note in the previous question
+
